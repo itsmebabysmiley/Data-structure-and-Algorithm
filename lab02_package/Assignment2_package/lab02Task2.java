5167640d53ed4d73706b62628ef35f3a5a6cf479
baby <snzzare@gmail.com>
2020-09-07 22:40:34 +0700

    delete lab02

--- lab02_package/Assignment2_package/lab02Task2.java
+++
@@ -1,8 +0,0 @@
-package Assignment2_package;
-
-public class lab02Task2 {
-   public static void main(String[] args) {
-    Reverser revObj = new Reverser("ComputereScience");
-    System.out.println(revObj.doRev());
-   }
-}

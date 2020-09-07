ed46029c92c447d65eedbbf132f44b37c2da2a96
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2020-09-07 22:28:44 +0700

    Add files via upload

---
+++ lab02_package/Assignment2_package/lab02Task1.java
@@ -0,0 +1,14 @@
+package Assignment2_package;
+
+public class lab02Task1 {
+    public static void main(String[] args) {
+        /* StackX stack = new StackX(50);
+        stack.push(5);
+        stack.push(15);
+        stack.push(25);
+        stack.push(30);
+        while(!stack.isEmpty()){
+            System.out.println(stack.pop());
+        } */
+    }
+}
---
+++ lab02_package/Assignment2_package/lab02Task2.java
@@ -0,0 +1,8 @@
+package Assignment2_package;
+
+public class lab02Task2 {
+   public static void main(String[] args) {
+    Reverser revObj = new Reverser("ComputereScience");
+    System.out.println(revObj.doRev());
+   }
+}
